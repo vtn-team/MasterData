564ce31ff5a09ba87a36428f1c131e1c05445ab7
k.mitarai <kazmit299^gmail.com>
2021-06-25 22:30:46 +0900

    同様のScriptableObjectを用意してみた

---
+++ Assets/Scripts/Game/CardData.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CardData", menuName = "ScriptableObjects/CardData", order = 1)]
+public class CardData : ScriptableObject
+{
+    [Serializable]
+    public class EffectData
+    {
+        public int Id;
+        public string Text;
+    }
+
+    [SerializeField] int Id;
+    [SerializeField] string Name;
+    [SerializeField] int Cost;
+    [SerializeField] int Power;
+    [SerializeField] int Toughness;
+    [SerializeField] List<EffectData> Effect;
+
+
+
+#if UNITY_EDITOR
+    public void SetData(int cost, int power, int toughness)
+    {
+        Cost = cost;
+        Power = power;
+        Toughness = toughness;
+    }
+#endif
+}
